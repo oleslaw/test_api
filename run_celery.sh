8ed006bbba3710470c82ec6c64e452ae3a769659
Aleksander Winecki <aleksander.winecki@gemius.com>
2025-07-16 23:01:14 +0200

    Limit celery memory usage

--- run_celery.sh
+++ run_celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-celery -A api worker --loglevel=info &
+celery -A api worker --loglevel=info --concurrency=1 --max-tasks-per-child=10 &
 celery -A api beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
