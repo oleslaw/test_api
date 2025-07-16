ad1f691c85b7e86088796d45e24b7a8ae11d0144
Aleksander Winecki <aleksander.winecki@gemius.com>
2025-07-16 23:12:25 +0200

    Add default value for phone number

--- api/contacts/models.py
+++ api/contacts/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=20, blank=True, default="")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} <{self.email}>"
